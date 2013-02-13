5d87dff9ca4079a09a5e5e0c4961a7938e23700f
Jan Hančič <jan.hancic@gmail.com>
2013-02-13 14:20:58 +0100

    add explanation for the `.sort()` wat

--- wat.js
+++ wat.js
@@ -12,3 +12,10 @@
 {} + []; // 0
 {} + {}; // NaN
 [0, -1, -2].sort(); // [-1, -2, 0]
+/*
+  Explanation:
+  `.sort()` converts elements to strings before comparing them, 
+  then it uses lexicographical sorting ("70" comes before "8" etc.).
+  If you want numerical sorting you have to provide your own `compareFunction` as detailed here: 
+  https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/sort
+*/
